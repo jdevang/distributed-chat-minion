ae5b94802f5548c407b7312c6e1ecd116bdc54ef
junglesucks <junglesucks2much@gmail.com>
2023-04-01 15:46:36 -0400

    restful

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"distributed-chat/minion/db"
 	"distributed-chat/minion/structs"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -9,9 +11,20 @@ import (
 type Message = structs.Message
 type User = structs.User
 
+var dbInstance = db.InitDb()
+
 func main() {
 	router := gin.Default()
-	router.GET("/register")
+	router.POST("/register", register)
 
 	router.Run("localhost:8080")
+}
+
+func register(c *gin.Context) {
+	var newUser User
+	if err := c.BindJSON(&newUser); err != nil {
+		return
+	}
+	db.CreateUser(dbInstance, newUser)
+	c.IndentedJSON(http.StatusCreated, newUser)
 }
